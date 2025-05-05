a5114c138f9dc4a0ccc5efb098f4e6296c5a0c7c
allisonochoa <allisonochoaolmedo@gmail.com>
2025-05-05 11:16:06 -0600

    Update Reviews_and_Ratings.sql

--- Dashboard_Queries/Reviews_and_Ratings.sql
+++ Dashboard_Queries/Reviews_and_Ratings.sql
@@ -22,38 +22,41 @@ LIMIT
 --Top 10 Best Selling Products AVG Rating (by volume)--
 WITH product_sales AS (
     SELECT
-        p.product_name,
-        p.product_id,
-        SUM(oi.quantity) AS total_quantity
+        products.product_name,
+        products.product_id,
+        SUM(order_items.quantity) AS total_quantity
     FROM
-        order_items oi
+        order_items
     JOIN
-        products p ON p.product_id = oi.product_id
+        products
+        ON products.product_id = order_items.product_id
     GROUP BY
-        p.product_id, p.product_name
+        products.product_id, products.product_name
 ),
 product_reviews AS (
     SELECT
-        p.product_name,
-        AVG(r.rating) AS average_rating
+        products.product_name,
+        AVG(reviews.rating) AS average_rating
     FROM
-        products p
+        products
     JOIN
-        reviews r ON r.product_id = p.product_id
+        reviews
+        ON reviews.product_id = products.product_id
     GROUP BY
-        p.product_name
+        products.product_name
 )
 
 SELECT
-    ps.product_name,
-    SUM(ps.total_quantity) AS total_quantity,
-    ROUND(pr.average_rating, 0) AS average_rating
+    product_sales.product_name,
+    SUM(product_sales.total_quantity) AS total_quantity,
+    ROUND(product_reviews.average_rating, 0) AS average_rating
 FROM
-    product_sales ps
+    product_sales
 LEFT JOIN
-    product_reviews pr ON ps.product_name = pr.product_name
+    product_reviews
+    ON product_sales.product_name = product_reviews.product_name
 GROUP BY
-    ps.product_name, pr.average_rating
+    product_sales.product_name, product_reviews.average_rating
 ORDER BY
     total_quantity DESC
 LIMIT 10;
